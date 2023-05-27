7b783e300bda489862dc0138265ef812ef466165
Karen Luna <karenlunafeldheim@gmail.com>
2023-05-26 22:09:44 -0300

    Funcionalidad con javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Links activos */
 document.addEventListener("DOMContentLoaded", function () {
   var navBar = document.querySelector(".contenedor-head .navbar-nav");
   var links = navBar.getElementsByClassName("nav-link");
@@ -16,3 +17,43 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+/*Desactivar flechas de incremento y decremento*/
+
+var numberInput = document.getElementById("total");
+numberInput.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+    event.preventDefault();
+  }
+});
+
+/**/
+
+let valorEntrada = 200;
+let cantidadEntradas = document.getElementById("cantidad");
+let categoria = document.getElementById("categoria");
+let compraTotal = document.getElementById("total");
+
+document.getElementById("resumen").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  let totalFinal = 0;
+
+  if (categoria === "estudiante") {
+    totalFinal = cantidadEntradas * valorEntrada * 0.2;
+    console.log(totalFinal);
+  } else if (categoria === "trainee") {
+    totalFinal = cantidadEntradas * valorEntrada * 0.5;
+    console.log(totalFinal);
+  } else if (categoria === "junior") {
+    totalFinal = cantidadEntradas * valorEntrada * 0.15;
+    console.log(totalFinal);
+  }
+
+  compraTotal.textContent = "Total a pagar: $" + totalFinal.toFixed(2);
+  console.log(
+    (compraTotal.textContent = "Total a pagar: $" + totalFinal.toFixed(2))
+  );
+});
+
+alert("Hola");
